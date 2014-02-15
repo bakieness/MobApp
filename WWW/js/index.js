4f509b14dc771df20523262466b86ad09d8d768c
bakieness <BAKJ5_11@uni.worc.ac.uk>
2014-02-15 16:10:28 +0000

    multiple additions

--- WWW/js/index.js
+++ WWW/js/index.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+
+function Afunction()
+{
+	var div = document.getElementById( 'alarm' );
+	var $color = div.style.backgroundColor;
+	div.onmouseover = function() 
+	{
+  		this.style.backgroundColor = 'green';
+  	}
+	div.onmouseout = function() 
+	{
+  		this.style.backgroundColor = $color;
+  	}
+}
