98991b9cd065fd2eac615f23a822c1bde9c58a97
bakieness <BAKJ5_11@uni.worc.ac.uk>
2014-02-16 15:19:32 +0000

    updated pages, added details page, localstorage added in .js file

--- WWW/js/index.js
+++ WWW/js/index.js
@@ -2,14 +2,36 @@
 
 function Afunction()
 {
-	var div = document.getElementById( 'alarm' );
-	var $color = div.style.backgroundColor;
-	div.onmouseover = function() 
+	var $div = document.getElementById('alarm');
+	var $color = $div.style.backgroundColor;
+	$div.onmouseover = function() 
 	{
   		this.style.backgroundColor = 'green';
   	}
-	div.onmouseout = function() 
+	$div.onmouseout = function() 
 	{
   		this.style.backgroundColor = $color;
   	}
+}
+
+function setVars()
+{
+	var _event = document.getElementById("event").value;	
+	window.localStorage.setItem("event", _event);	
+	var _type = document.getElementById("type").value;	
+	window.localStorage.setItem("type", _type);	
+}
+
+function Sfunction()
+{
+	var $type = window.localStorage.getItem("event");
+	var $name = window.localStorage.getItem("type");
+	if ($type != null)
+		{
+		document.getElementById("p1").innerHTML=$type;
+		}
+	if ($name != null)
+		{
+		document.getElementById("p2").innerHTML=$name;
+		}
 }
