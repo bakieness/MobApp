17adf8fee87a7ed288637ef4983a0beee7588dc0
bakieness <BAKJ5_11@uni.worc.ac.uk>
2014-03-04 16:34:56 +0000

    adding info to details page & repeat input for addform page

--- WWW/js/index.js
+++ WWW/js/index.js
@@ -1,33 +1,29 @@
 // JavaScript Document
-ArrayList<ArrayList<String>> myList;
+ArrayList<ArrayList<String>> myAlarms;
+
 function setVars()
 {
 	var _event = document.getElementById("event").value;	
 	var _type = document.getElementById("type").value;	
 	var _date = document.getElementById("date").value;
 	var _time = document.getElementById("time").value;	
+	var _repeat = document.getElementById("repeat").value;
 	window.localStorage.setItem("event", _event);	
 	window.localStorage.setItem("type", _type);
 	window.localStorage.setItem("date", _date);
 	window.localStorage.setItem("time", _time);
-	
-	
-	
-//	for (var i=0; i<=100; i++)
-//	{
-//		myList.get(0).set(0, _event);
-//		myList.get(0).set(1, _type);
-//		myList.get(0).set(2, _date);
-//		myList.get(0).set(3, _time);
-//		if (alarms[i] == null)
-//		{
-//			alarms[i] = new array();
-//			alarms[i][0] = _event;
-//			alarms[i][1] = _type;
-//			alarms[i][2] = _date;
-//			alarms[i][3] = _time;
-//		}
-//	}
+	window.localStorage.setItem("repeat", _repeat);
+		
+	for (var i=0; i<myAlarms.size(); i++)
+	{
+		if (myAlarms.get(i) = null)
+		{
+			myAlarms.get(i).set(0, _event);
+			myAlarms.get(i).set(1, _type);
+			myAlarms.get(i).set(2, _date);
+			myAlarms.get(i).set(3, _time);
+		}
+	}
 }
 
 function Sfunction()
@@ -35,7 +31,8 @@ function Sfunction()
 	var $event = window.localStorage.getItem("event");
 	var $type = window.localStorage.getItem("type");
 	var $date = window.localStorage.getItem("date");
-	var $time = window.localStorage.getItem("time");	 
+	var $time = window.localStorage.getItem("time");
+	var $repeat = window.localStorage.getItem("repeat");	 
 	
 	if ($event != null)
 		{
@@ -76,4 +73,34 @@ function Sfunction()
 		$div.style.backgroundColor = '#FBEB91';
 		break;
 	}
+}
+
+function Sfunction2()
+{
+	var $event = window.localStorage.getItem("event");
+	var $type = window.localStorage.getItem("type");
+	var $date = window.localStorage.getItem("date");
+	var $time = window.localStorage.getItem("time");
+	var $repeat = window.localStorage.getItem("repeat");	 
+	
+	if ($event != null)
+		{
+			document.getElementById("p1").innerHTML=$event;
+		}
+	if ($type != null)
+		{
+			document.getElementById("p2").innerHTML=$type;
+		}
+	if ($date != null)
+		{
+			document.getElementById("p3").innerHTML=$date;
+		}
+	if ($time != null)
+		{
+			document.getElementById("p4").innerHTML=$time;
+		}
+	if ($repeat != null)
+		{
+			document.getElementById("p5").innerHTML=$repeat;
+		}	
 }
