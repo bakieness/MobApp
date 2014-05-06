84d692be2714eb465c655b07505204d49772f55e
bakieness <BAKJ5_11@uni.worc.ac.uk>
2014-05-06 15:31:29 +0100

    stuff

--- WWW/js/database.js
+++ WWW/js/database.js
@@ -1,11 +1,11 @@
 // JavaScript Document
-var db = window.openDatabase('myDB', '1.0', 'myDb', 1024 * 1024 * 500);
-	if (window.localStorage.getItem("new") === null)
-	{
+var db = window.openDatabase('myDB2', '1.0', 'myDb', 1024 * 1024 * 500);
 		db.transaction(function(tx) 
 		{
 			tx.executeSql('CREATE TABLE ALARMS (id unique, title, type, date, time, repeat)');
 		});
+	if (window.localStorage.getItem("new") === null)
+	{
 		window.localStorage.setItem("new", "notnow");
 		window.localStorage.setItem("i", 0);
 	}
@@ -13,6 +13,7 @@ var db = window.openDatabase('myDB', '1.0', 'myDb', 1024 * 1024 * 500);
 
 function AddDB()
 {
+	var db = window.openDatabase('myDB2', '1.0', 'myDb', 1024 * 1024 * 500);
 	var $event = window.localStorage.getItem("event");
 	var $type = window.localStorage.getItem("type");
 	var $date = window.localStorage.getItem("date");
@@ -34,19 +35,18 @@ function GetAlarms()
 	{
 		tx.executeSql('SELECT * FROM ALARMS', [], function (tx, results)
 		{
-			
 			var len = results.rows.length, i;
 			for (i = 0; i < len; i++)
 			{	
-			var thing = document.createElement("div")
-			thing.setAttribute('id', i);
-			thing.setAttribute('onClick', 'window.location = "index.html#Details"; ShowDetails(this)');
+			var newDiv = document.createElement("div")
+			newDiv.setAttribute('id', i);
+			newDiv.setAttribute('onClick', 'window.location = "index.html#Details"; ShowDetails(this)');
 			
 			var a = document.createElement("p");
 			var b = document.createElement("p");
 			var c = document.createElement("p");
 			var d = document.createElement("p");
-			-
+			
 			a.setAttribute('id', 'p1');
 			b.setAttribute('id', 'p2');
 			c.setAttribute('id', 'p3');
@@ -62,79 +62,123 @@ function GetAlarms()
 			c.appendChild(w);
 			d.appendChild(e);
 			
-			thing.appendChild(a);
-			thing.appendChild(b);
-			thing.appendChild(c);
-			thing.appendChild(d);
+			newDiv.appendChild(a);
+			newDiv.appendChild(b);
+			newDiv.appendChild(c);
+			newDiv.appendChild(d);
 			
 			if (results.rows.item(i).title == null && results.rows.item(i).type == null)
 			{
-				thing.style.display = 'none';
+				newDiv.style.display = 'none';
 			}
 			
 			switch (results.rows.item(i).type)
 			{
 				case "Assignment":
-					thing.style.backgroundColor = '#FB9B9B';
+					newDiv.style.backgroundColor = '#FB9B9B';
 					break;
 				case "Lecture":
-					thing.style.backgroundColor = '#A2BEF4';
+					newDiv.style.backgroundColor = '#A2BEF4';
 					break;
 				case "Social":
-   					thing.style.backgroundColor = '#A2FA92';
+   					newDiv.style.backgroundColor = '#A2FA92';
   					break;
 				case "Other":
-					thing.style.backgroundColor = '#FBEB91';
+					newDiv.style.backgroundColor = '#FBEB91';
 					break;
 			}			
 			$content = document.getElementById("contenthome");
-			$content.appendChild(thing);
+			$content.appendChild(newDiv);
 			}
 		})
 	})
 }
 
-function deleteAlarm(aid)
-{
-	alert(aid);
-/*	db.transaction(function (tx) 
-	{
-		tx.executeSql('DELETE * FROM ALARMS WHERE id = ?', [aid], function (tx, results)
-		{
-			alert("ALARM DELETED");
-		})
-	});*/
-}
-
 function ShowDetails(alarm)
-{
+{	
+	var aid = alarm.id;
+	var title = document.createTextNode("Title:");
 	var p1 = document.createTextNode(alarm.getElementsByTagName('p')[0].innerHTML);
+	var type = document.createTextNode("Type:");
 	var p2 = document.createTextNode(alarm.getElementsByTagName('p')[1].innerHTML);
+	var date = document.createTextNode("Date:");
 	var p3 = document.createTextNode(alarm.getElementsByTagName('p')[2].innerHTML);
+	var time = document.createTextNode("Time:");
 	var p4 = document.createTextNode(alarm.getElementsByTagName('p')[3].innerHTML);
 	
-	//do styling here like above
-	$content = document.getElementById("details");
-	$content.appendChild(p1);
-	$content.appendChild(p2);
-	$content.appendChild(p3);
-	$content.appendChild(p4);
+	var newDiv = document.createElement("div");
 	
-	var aid = alarm.id;
-	alert(aid);
-	db.transaction(function (tx) 
-	{
-		tx.executeSql('SELECT * FROM ALARMS WHERE (id = ?)', [aid], function (tx, results)
-		{
-			alert(results.rows.length);
-			for(var i=0; i<results.rows.length; i++) 
-			{	
-			var details = document.createTextNode(results.rows.item(i).type);
-			alert(results.rows.item(i).type);
-			}
-		})
-	});
+	var a = document.createElement("p");
+	var b = document.createElement("p");
+	var c = document.createElement("p");
+	var d = document.createElement("p");
+	var e = document.createElement("p");
+	var f = document.createElement("p");
+	var g = document.createElement("p");
+	var h = document.createElement("p");
+	
+	a.style.fontWeight = "bold";
+	c.style.fontWeight = "bold";
+	e.style.fontWeight = "bold";
+	g.style.fontWeight = "bold";
+	
+	a.style.textAlign = "center";
+	b.style.textAlign = "center";
+	c.style.textAlign = "center";
+	d.style.textAlign = "center";
+	e.style.textAlign = "center";
+	f.style.textAlign = "center";
+	g.style.textAlign = "center";
+	h.style.textAlign = "center";
+	
+	a.appendChild(title);
+	b.appendChild(p1);
+	c.appendChild(type);
+	d.appendChild(p2);
+	e.appendChild(date);
+	f.appendChild(p3);
+	g.appendChild(time);
+	h.appendChild(p4);
+	
+	newDiv.appendChild(a);
+	newDiv.appendChild(b);
+	newDiv.appendChild(c);
+	newDiv.appendChild(d);
+	newDiv.appendChild(e);
+	newDiv.appendChild(f);
+	newDiv.appendChild(g);
+	newDiv.appendChild(h);
+	
+	$content = document.getElementById("details");
+	$content.innerHTML = "";
+	$content.appendChild(newDiv);
 	
 	del = document.getElementById("delbtn");
-	del.setAttribute('onClick', 'window.location = "index.html"; deleteAlarm(aid)');
+	del.setAttribute('onClick', 'window.location = "index.html"; deleteAlarm()');
+}
+
+
+function deleteAlarm()
+{
+	var db = window.openDatabase('myDB2', '1.0', 'myDb', 1024 * 1024 * 500);
+	alert("hi");
+	alert(db);
+	
+	db.transaction(function (tx) 
+	{
+		var aid = "3";
+		alert(aid);
+		alert(tx);
+		tx.executeSql('DELETE FROM ALARMS', [], 
+			function (tx, results)
+			{
+				alert("ALARM DELETED");
+			},
+			function (error)
+			{
+				alert("fh");
+			}
+		)
+	});
+	alert("hf")
 }
