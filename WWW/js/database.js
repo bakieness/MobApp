160771c5d31f7348834a554dbe681f578aa764ec
bakieness <BAKJ5_11@uni.worc.ac.uk>
2014-05-15 18:32:52 +0100

    test 1

--- WWW/js/database.js
+++ WWW/js/database.js
@@ -184,7 +184,9 @@ function setid(div)
 	$(document.body).click(function(evt)
 	{
 		var clicked = evt.target;
+		alert(clicked);
 		var currentID = clicked.id;
+		alert(currentID);
 		var text = $.trim($('#' + currentID).text()),
     	word = text.split(' ');
     	dataid = word[0];
