c07427455d4d1772f6e3185d4cd9ba4f0b63a62e
bakieness <BAKJ5_11@uni.worc.ac.uk>
2014-05-16 15:23:22 +0100

    jef

--- WWW/js/database.js
+++ WWW/js/database.js
@@ -46,7 +46,7 @@ function onDeviceReady() {
 						}
 						});
 					});
-			}, 60 * 1000);
+			}, 55 * 1000);
 			
 			//Once table is created this function is called.
 			//This function must be here to gurantee it runs after database transactionthis is because database transactions are asynchronous.
