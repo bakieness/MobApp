b847773baadd475903afba93e3187801912aab61
bakieness <BAKJ5_11@uni.worc.ac.uk>
2014-05-16 11:31:04 +0100

    ahhhhhhh

--- WWW/js/database.js
+++ WWW/js/database.js
@@ -336,7 +336,7 @@ function date()
 
 function datePlusWeek()
 {
-	var date = new Date();
+	var today = new Date();
 	var dd = today.getDate();
 	var mm = today.getMonth()+1; //January is 0
 	var yyyy = today.getFullYear();
